0e21125c02f182f3e4ecec02b95ed26ba49ddbac
p1ngwin <first.rok@gmail.com>
2023-07-24 16:05:18 +0200

    server config

--- server/server.ts
+++ server/server.ts
@@ -13,13 +13,31 @@ app.use(express.json());
 
 const defaultPort = process.env.SERVER_PORT
   ? parseInt(process.env.SERVER_PORT, 10)
-  : 80;
+  : 3000;
 const env = process.env.ENVIRONMENT;
-const port = env !== "local" ? defaultPort : 8000;
+const port = env !== "local" ? defaultPort : 3000;
 
 const uri = String(process.env.DATABASE_CONNECTION);
 
-mongoose.connect(uri).then((res) => console.log("Connected to DB!"));
+mongoose.set("strictQuery", true);
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  autoIndex: false,
+  serverSelectionTimeoutMS: 5000,
+  socketTimeoutMS: 45000,
+  family: 4,
+  bufferCommands: true,
+};
+
+mongoose.connect(uri, options).then(
+  () => {
+    console.log("Database connection established!");
+  },
+  (err) => {
+    console.log("Error connecting Database instance due to: ", err);
+  }
+);
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
