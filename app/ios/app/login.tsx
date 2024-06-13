128deabc6d9243baf53431535ece0718cfef9946
p1ngwin <first.rok@gmail.com>
2024-06-13 10:31:40 +0200

    IOS react native init

---
+++ app/ios/app/login.tsx
@@ -0,0 +1,12 @@
+import { ThemedView } from '@/components/ThemedView';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export default function Login() {
+  const a = 1;
+
+  return (
+    <SafeAreaView>
+      <ThemedView></ThemedView>
+    </SafeAreaView>
+  );
+}
